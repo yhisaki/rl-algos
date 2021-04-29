d75a4010ec707579831256575fc6875b07c7671f
Y.Hisaki <yhisaki31@gmail.com>
2021-04-29 16:21:29 +0900

    update init.py

--- rlrl/nn/__init__.py
+++ rlrl/nn/__init__.py
@@ -1,1 +1,2 @@
 from rlrl.nn.lmbda import Lambda
+from rlrl.nn.build_simple_linear_nn import build_simple_linear_nn
--- rlrl/policies/__init__.py
+++ rlrl/policies/__init__.py
@@ -1,1 +1,1 @@
-from rlrl.policies.squashed_gaussian_policy import SquashedGaussianPolicy
+from rlrl.policies.squashed_gaussian_policy import squashed_diagonal_gaussian_head, SquashedGaussianPolicy
