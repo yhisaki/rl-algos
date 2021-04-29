8789dbb455f1857ef153ce1c474b8c507a39b408
Y.Hisaki <yhisaki31@gmail.com>
2021-04-29 16:21:09 +0900

    update nn builder

--- rlrl/nn/build_simple_linear_nn.py
+++ rlrl/nn/build_simple_linear_nn.py
@@ -8,8 +8,20 @@ def build_simple_linear_nn(
     output_dim: int,
     hidden_units: list,
     hidden_activation,
-    output_activation: nn.Module = None
+    output_activation = None
 ):
+  """build_simple_linear_nn
+
+  Args:
+      input_dim (int): [description]
+      output_dim (int): [description]
+      hidden_units (list): [description]
+      hidden_activation ([type]): [description]
+      output_activation ([type], optional): [description]. Defaults to None.
+
+  Returns:
+      Linear Neural Network
+  """
   model = []
   units = input_dim
   for next_units in hidden_units:
@@ -20,6 +32,8 @@ def build_simple_linear_nn(
 
   if isinstance(output_activation, types.FunctionType):
     model.append(Lambda(output_activation))
+  elif output_activation is None:
+    pass
   elif issubclass(output_activation, nn.Module):
     model.append(output_activation())
 
