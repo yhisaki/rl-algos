08054282d6a731566331ae0fb51c6ade14266639
Y.Hisaki <yhisaki31@gmail.com>
2021-05-12 00:26:18 +0900

    delete binary_heap

--- rlrl/collections/binary_heap.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import heapq
-
-class EntropyPrioritizedExperience(object):
-  def __init__(self, *args, **kwargs):
-    self.maxlen = kwargs.pop("maxlen", None)
-    assert self.maxlen is None or self.maxlen >= 0
-
-  # def add(self, )
