bfaa035ae3132810caf0450ad486518f74bbabda
Y.Hisaki <yhisaki31@gmail.com>
2021-06-19 21:43:32 +0900

    update nn.contexts

--- rlrl/nn/contexts.py
+++ rlrl/nn/contexts.py
@@ -1,20 +1,32 @@
 from contextlib import contextmanager
+from rlrl.utils.get_module_device import get_module_device
 
-# It was implemented using https://github.com/pfnet/pfrl/blob/master/pfrl/utils/contexts.py
+# It was implemented using https://github.com/pfm/pfrl/blob/master/pfrl/utils/contexts.py
 # as a reference.
 
 
 @contextmanager
-def evaluating(*nets):
+def evaluating(*ms):
     """Temporarily switch to evaluation mode."""
-    istrains = []
-    for net in nets:
-        istrains.append(net.training)
+    istrains = [m.training for m in ms]
     try:
-        for net in nets:
-            net.eval()
-            yield net
+        for m in ms:
+            m.eval()
+            yield m
     finally:
-        for net, istrain in zip(nets, istrains):
+        for m, istrain in zip(ms, istrains):
             if istrain:
-                net.train()
+                m.train()
+
+
+@contextmanager
+def device_placement(dev, *ms):
+    """Temporarily place m on the specified device."""
+    pre_devs = [get_module_device(m) for m in ms]
+    try:
+        for m in ms:
+            m.to(dev)
+            yield m
+    finally:
+        for m, dev in zip(ms, pre_devs):
+            m.to(dev)
