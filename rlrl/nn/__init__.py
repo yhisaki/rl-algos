59e02a3c28a21fe418dd0f108143a77d1823dc58
Y.Hisaki <yhisaki31@gmail.com>
2021-06-20 01:30:39 +0900

    update init.py and eval_determistic_policy

--- rlrl/nn/__init__.py
+++ rlrl/nn/__init__.py
@@ -1,5 +1,6 @@
 from rlrl.nn.lmbda import Lambda
 from rlrl.nn.build_simple_linear_sequential import build_simple_linear_sequential
 from rlrl.nn.contexts import evaluating
+from rlrl.nn.contexts import device_placement
 
-__all__ = ["Lambda", "build_simple_linear_sequential", "evaluating"]
+__all__ = ["Lambda", "build_simple_linear_sequential", "evaluating", "device_placement"]
