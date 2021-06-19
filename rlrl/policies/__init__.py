e60a488b1eeb49e415d4dfe04672809aafa7873d
Y.Hisaki <yhisaki31@gmail.com>
2021-06-19 21:44:56 +0900

    add determistic_squashed_gaussian_policy

--- rlrl/policies/__init__.py
+++ rlrl/policies/__init__.py
@@ -1,9 +1,11 @@
 from rlrl.policies.squashed_gaussian_policy import (
     squashed_diagonal_gaussian_head,
     SquashedGaussianPolicy,
+    determistic_squashed_gaussian_policy
 )
 
 __all__ = [
     "squashed_diagonal_gaussian_head",
     "SquashedGaussianPolicy",
+    "determistic_squashed_gaussian_policy"
 ]
--- rlrl/policies/squashed_gaussian_policy.py
+++ rlrl/policies/squashed_gaussian_policy.py
@@ -1,4 +1,6 @@
 import copy
+from contextlib import contextmanager
+from rlrl.nn.lmbda import Lambda
 import torch
 import torch.nn as nn
 from torch import distributions
@@ -33,6 +35,39 @@ def squashed_diagonal_gaussian_head(x):
         distributions.Normal(loc=mean, scale=torch.sqrt(var)), 1
     )
     # cache_size=1 is required for numerical stability
+    # https://pytorch.org/docs/stable/distributions.html#torch.distributions.transformed_distribution.TransformedDistribution
     return distributions.transformed_distribution.TransformedDistribution(
         base_distribution, [distributions.transforms.TanhTransform(cache_size=1)]
     )
+
+
+def determistic_squashed_gaussian_policy_head(x):
+    mean, _ = torch.chunk(x, 2, dim=x.dim() // 2)
+    return torch.tanh(mean)
+
+
+@contextmanager
+def determistic_squashed_gaussian_policy(policy: nn.Sequential):
+    """Temporaliy switch squashed gaussian policy to determistic policy
+
+    Args:
+        policy (nn.Sequential): [description]
+
+    Yields:
+        [type]: [description]
+    """
+    try:
+        del policy[-1]
+        head = Lambda(determistic_squashed_gaussian_policy_head)
+        policy.add_module(
+            head.__repr__(),
+            head,
+        )
+        yield policy
+    finally:
+        del policy[-1]
+        head = Lambda(squashed_diagonal_gaussian_head)
+        policy.add_module(
+            head.__repr__(),
+            head,
+        )
