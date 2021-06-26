b828fa314642a23e8901615159231359408aba46
Y.Hisaki <yhisaki31@gmail.com>
2021-06-26 13:40:38 +0900

    update get_module_device

--- rlrl/utils/get_module_device.py
+++ rlrl/utils/get_module_device.py
@@ -1,4 +1,11 @@
+from typing import List
+
+import torch
 import torch.nn as nn
+
+
+def _all_the_same(elements):
+    return elements[1:] == elements[:-1]
 
 
 def get_module_device(m: nn.Module):
@@ -10,4 +17,11 @@ def get_module_device(m: nn.Module):
     Returns:
         device
     """
-    return next(m.parameters()).device
+    devices: List[torch.device] = []
+    for p in m.parameters():
+        devices.append(p.device)
+
+    if not _all_the_same(devices):
+        raise ValueError("Not all parameters are placed on the same device.")
+
+    return devices[0]
