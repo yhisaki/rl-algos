3a342534616b8b24fbe934470c24927057b3d401
Y.Hisaki <yhisaki31@gmail.com>
2021-07-10 00:54:38 +0900

    update munual_seed

--- rlrl/utils/manual_seed.py
+++ rlrl/utils/manual_seed.py
@@ -1,10 +1,16 @@
 import random
+from typing import Optional
 
 import numpy as np
 import torch
 
 
-def manual_seed(seed: int = 0):
-    torch.manual_seed(seed)
-    random.seed(seed)
-    np.random.seed(seed)
+def manual_seed(
+    seed: int = 0,
+    torch_seed: Optional[int] = None,
+    random_seed: Optional[int] = None,
+    np_seed: Optional[int] = None,
+):
+    torch.manual_seed(seed if torch_seed is None else torch_seed)
+    random.seed(seed if random_seed is None else random_seed)
+    np.random.seed(seed if np_seed is None else np_seed)
