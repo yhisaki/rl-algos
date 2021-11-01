ec4d4e11849585a46eb9df39d9c01394a14f5efd
Y.Hisaki <yhisaki31@gmail.com>
2021-11-01 23:54:56 +0900

    update agents/__init__.py

--- rlrl/agents/__init__.py
+++ rlrl/agents/__init__.py
@@ -1,5 +1,7 @@
 from rlrl.agents.agent_base import AgentBase
-from rlrl.agents.sac_agent import SacAgent, SquashedDiagonalGaussianHead
+from rlrl.agents.sac_agent import SacAgent
 from rlrl.agents.trpo_agent import TrpoAgent
+from rlrl.agents.atrpo_agent import AtrpoAgent
+from rlrl.agents.td3_agent import Td3Agent
 
-__all__ = ["AgentBase", "SacAgent", "TrpoAgent", "SquashedDiagonalGaussianHead"]
+__all__ = ["AgentBase", "SacAgent", "TrpoAgent", "AtrpoAgent", "Td3Agent"]
