3e57f00b533ed14f8b2d3acf6d68278469078cd4
hisaki <yhisaki31@gmail.com>
2021-12-28 10:45:32 +0900

    change file names

--- rlrl/experiments/__init__.py
+++ rlrl/experiments/__init__.py
@@ -1,5 +1,5 @@
 from rlrl.experiments.evaluator import Evaluator
-from rlrl.experiments.gym_mdp import TransitionGenerator
+from rlrl.experiments.transition_generator import TransitionGenerator
 from rlrl.experiments.recorder import Recoder
 
 __all__ = ["TransitionGenerator", "Recoder", "Evaluator"]
